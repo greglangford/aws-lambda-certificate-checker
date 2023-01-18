643bf683dd5434df457702c9c62e07c4e6bff7b1
Greg Langford <greg@langford.me>
2023-01-18 23:00:00 +0000

    Various updates

--- checker/app.py
+++ checker/app.py
@@ -7,44 +7,44 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    hostname = '1.2.3.4'
+    host = 'greglangford.co.uk'
     port = 443
     expiry_days = 14
 
     try:
-        logger.info(f"Attempting to get certificate for hostname {hostname} on port {port}")
-        certificate = PeerCertificate(hostname, port)
+        logger.info(f"Attempting to get certificate for host {host} on port {port}")
+        certificate = PeerCertificate(host, port)
 
-        if certificate.match_hostname():
-            logger.info(f"Certificate matches the hostname {hostname}")
+        if certificate.match_host():
+            logger.info(f"Certificate matches the host {host}")
 
         if not certificate.is_expiring(days=expiry_days):
-            logger.info(f"Certificate for hostname {hostname} is not due to expire within {expiry_days} days")
+            logger.info(f"Certificate for host {host} is not due to expire within {expiry_days} days")
     except PeerCertificateException as e:
         logger.error(e)
 
 class PeerCertificate():
-    def __init__(self, hostname, port):
+    def __init__(self, host, port):
         
-        self.hostname = hostname
+        self.host = host
         self.port = port
 
         try:
             context = ssl.create_default_context()
-            context.check_hostname = False
+            context.check_host = False
 
-            with socket.create_connection((self.hostname, self.port), timeout=30) as sock:
-                with context.wrap_socket(sock, server_hostname=self.hostname) as ssock:
+            with socket.create_connection((self.host, self.port), timeout=30) as sock:
+                with context.wrap_socket(sock, server_hostname=self.host) as ssock:
                     certificate = ssock.getpeercert()
 
                     if certificate:
                         self.certificate = certificate
 
         except socket.timeout:
-            raise PeerCertificateException(f"Time out connecting to {hostname} on port {port}")
+            raise PeerCertificateException(f"Timeout connecting to {host} on port {port}")
         except ssl.SSLError as e:
             logger.debug(e)
-            raise PeerCertificateException(f"No certificate found for hostname {hostname} on port {port}")
+            raise PeerCertificateException(f"No certificate found for host {host} on port {port}")
 
     def is_expiring(self, days=14):
         """Raise a PeerCertificateException if certificate is expiring within 'days' or return False"""
@@ -53,7 +53,7 @@ class PeerCertificate():
             expiry_date_delta = expiry_date - datetime.timedelta(days=days)
 
             if datetime.datetime.utcnow() >= expiry_date_delta:
-                raise PeerCertificateException(f"The certificate for hostname {self.hostname} is expiring within {days} days")
+                raise PeerCertificateException(f"The certificate for host {self.host} is expiring within {days} days")
 
         except ValueError as e:
             logger.debug(e)
@@ -61,13 +61,13 @@ class PeerCertificate():
 
         return False
 
-    def match_hostname(self):
-        """Match the hostname to the certificate or throw a PeerCertificateException"""
+    def match_host(self):
+        """Match the host to the certificate or throw a PeerCertificateException"""
         try:
-            ssl.match_hostname(self.certificate, self.hostname)
+            ssl.match_hostname(self.certificate, self.host)
         except ssl.CertificateError as e:
             logger.debug(e)
-            raise PeerCertificateException(f"Hostname {self.hostname} does not match the certificate")
+            raise PeerCertificateException(f"host {self.host} does not match the certificate")
 
         return True
 
